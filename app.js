fb0b614f39925c52c2c514f06e49d280557e526b
Joseph Antonio Carita Mamani <joseph.cmvd@gmail.com>
2024-04-10 16:16:04 -0500

    marcado

--- app.js
+++ app.js
@@ -21,16 +21,22 @@ const ul = document.querySelector('ul')
 to_do.forEach(item => {
     console.log(item)
     let li = document.createElement('li')
-    let  p = document.createElement('p')
+    let p = document.createElement('p')
     let checkbox = document.createElement('input')
     checkbox.type = 'checkbox'
+    checkbox.addEventListener('change', function() {
+        if (this.checked) {
+            p.style.textDecoration = 'line-through';
+            p.style.color = 'gray';
+        } else {
+            p.style.textDecoration = 'none';
+            p.style.color = 'black';
+        }
+    });
     p.innerText = item
-    // checkbox.labels = item
     li.appendChild(checkbox)
     li.appendChild(p)
-    // li.innerHTML = item
     ul.appendChild(li)
 })
 
-console.log('tes')
 
